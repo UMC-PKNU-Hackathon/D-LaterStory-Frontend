11843384f2c7f26a8310ff8af39fdf25acddda92
03hoho03 <ghwjd321@naver.com>
2023-11-24 20:30:44 +0900

    Feat 메인페이지

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import styled from 'styled-components'
+import MainSwiper from '../../components/MainPage/MainSwiper'
+import EntireReviewList from '../../components/MainPage/EntireReviewList'
+
+const MainWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`
+const SwiperWrapper = styled.div`
+  width: 100%;
+  margin-bottom: 24px;
+`
+const ContentWrapper = styled.div`
+  width: 100%;
+  max-width: 1080px;
+`
 
 const MainPage = () => {
-  return <div>MainPage</div>
+  return (
+    <MainWrapper>
+      <SwiperWrapper>
+        <MainSwiper />
+      </SwiperWrapper>
+      <ContentWrapper>
+        <EntireReviewList />
+      </ContentWrapper>
+    </MainWrapper>
+  )
 }
 
 export default MainPage
