c87c07a737580c40ef740d72192067bccf6e8fb0
03hoho03 <ghwjd321@naver.com>
2023-11-24 20:34:08 +0900

    Feat Layout 및 route 수정

--- src/App.js
+++ src/App.js
@@ -8,9 +8,16 @@ import SignupPage from './pages/SignupPage'
 import MainPage from './pages/MainPage'
 import ReviewPage from './pages/ReviewPage'
 import ProfilePage from './pages/ProfilePage'
+import NavigationHeader from './components/common/NavigationHeader'
+import SearchPage from './pages/SearchPage'
 
 function Layout() {
-  return <Outlet />
+  return (
+    <>
+      <NavigationHeader />
+      <Outlet />
+    </>
+  )
 }
 
 function App() {
@@ -18,10 +25,11 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<MainPage />} />
-        <Route path="post/:postId" element={<ReviewPage />} />
+        <Route path="/review/:reviewId" element={<ReviewPage />} />
+        <Route path="/search?" element={<SearchPage />} />
         <Route element={<NotAuthRoute />}>
           <Route path="/login" element={<SigninPage />} />
-          <Route path="signup" element={<SignupPage />} />
+          <Route path="/signup" element={<SignupPage />} />
         </Route>
         <Route element={<ProtectedRoute />}>
           <Route path="/profile/:userId" element={<ProfilePage />} />
