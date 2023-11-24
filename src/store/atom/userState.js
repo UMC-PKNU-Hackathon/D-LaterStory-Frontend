a321858a06dddc5a32c99c819b44f1aeaeb7c9da
03hoho03 <ghwjd321@naver.com>
2023-11-25 01:28:09 +0900

    Feat userState 전역

--- src/store/atom/userState.js
+++ src/store/atom/userState.js
@@ -1,8 +1,15 @@
 import { atom } from 'recoil'
+import { recoilPersist } from 'recoil-persist'
+
+const { persistAtom } = recoilPersist({
+  key: 'userState',
+  storage: localStorage,
+})
 
 const userState = atom({
   key: 'userState',
-  default: { user: null, isAuth: true },
+  default: { user: null, isAuth: false },
+  effects_UNSTABLE: [persistAtom],
 })
 
 export default userState
