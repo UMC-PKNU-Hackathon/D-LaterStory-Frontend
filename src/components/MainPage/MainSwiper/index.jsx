583eaa6ecbe0be734d0472d3c4ce277fb880ac42
03hoho03 <ghwjd321@naver.com>
2023-11-25 00:16:30 +0900

    Refact 슬라이더 기능변경

--- src/components/MainPage/MainSwiper/index.jsx
+++ src/components/MainPage/MainSwiper/index.jsx
@@ -1,28 +1,76 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Navigation, Pagination } from 'swiper/modules'
+import { Pagination, Autoplay } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
-import 'swiper/css/navigation'
+import 'swiper/css/pagination'
 
-const MockDiv = styled.div`
+const SlideWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 32px 0;
   width: 100%;
-  height: 400px;
-  background-color: black;
-  color: white;
+  height: 300px;
+  background-color: #f4c876;
+`
+const SlideContentWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  max-width: 1080px;
+`
+const SlideTextContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+const TextContentTitle = styled.h5`
+  font-size: 32px;
+  margin: 0;
+  margin-bottom: 16px;
+`
+const TextContent = styled.p`
+  display: flex;
+  align-items: center;
+  width: 300px;
+  margin: 0;
+  font-size: 16px;
+  font-weight: normal;
+`
+const SlideImageContent = styled.div`
+  width: 300px;
+  height: 100%;
+`
+const SlideImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
 `
 
 const MainSwiper = () => {
   return (
     <Swiper
-      modules={[Pagination, Navigation]}
+      modules={[Pagination, Autoplay]}
       slidesPerView={1}
       pagination={{ clickable: true }}
-      navigation
+      loop
+      autoplay={{ delay: 5000, disableOnInteraction: false }}
     >
-      {Array.from({ length: 5 }).map((_, idx) => (
+      {Array.from({ length: 1 }).map((_, idx) => (
         <SwiperSlide key={idx}>
-          <MockDiv>안녕</MockDiv>
+          <SlideWrapper>
+            <SlideContentWrapper>
+              <SlideTextContentWrapper>
+                <TextContentTitle>경험을 나눠봐요</TextContentTitle>
+                <TextContent>
+                  후일담으로 여러분들의 생생한 체험 후기를 전해보세요.
+                </TextContent>
+              </SlideTextContentWrapper>
+              <SlideImageContent>
+                <SlideImage src="./main_banner_01.png" alt="배너이미지" />
+              </SlideImageContent>
+            </SlideContentWrapper>
+          </SlideWrapper>
         </SwiperSlide>
       ))}
     </Swiper>
